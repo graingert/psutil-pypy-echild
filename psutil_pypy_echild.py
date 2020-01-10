3c7c8ffc672ec4e44a8d78dbfd7179d054b59801
Thomas Grainger <tagrain@gmail.com>
2020-01-10 10:07:08 +0000

    patch to demonstate isinstance working but `except:` not working

--- psutil_pypy_echild.py
+++ psutil_pypy_echild.py
@@ -4,6 +4,7 @@ import signal
 import sys
 import time
 
+import patchy
 import psutil
 
 try:
@@ -14,6 +15,41 @@ except ImportError:
 # Constants from prctl.h
 PR_GET_PDEATHSIG = 2
 PR_SET_PDEATHSIG = 1
+
+patchy.patch(
+    "psutil._psposix.wait_pid",
+    """
+    @@ -34,24 +34,28 @@
+             except ChildProcessError:
+                 # This has two meanings:
+                 # - pid is not a child of os.getpid() in which case
+                 #   we keep polling until it's gone
+                 # - pid never existed in the first place
+                 # In both cases we'll eventually return None as we
+                 # can't determine its exit status code.
+                 while True:
+                     if pid_exists(pid):
+                         delay = check_timeout(delay)
+                     else:
+                         return
+    +        except Exception as e:
+    +            if isinstance(e, ChildProcessError):
+    +                raise Exception("Something very strange happened")
+    +            raise
+             else:
+                 if retpid == 0:
+                     # WNOHANG was used, pid is still running
+                     delay = check_timeout(delay)
+                     continue
+                 # process exited due to a signal; return the integer of
+                 # that signal
+                 if os.WIFSIGNALED(status):
+                     return -os.WTERMSIG(status)
+                 # process exited using exit(2) system call; return the
+                 # integer exit(2) system call has been called with
+                 elif os.WIFEXITED(status):
+    """,
+)
 
 
 def set_pdeathsig(sig):
